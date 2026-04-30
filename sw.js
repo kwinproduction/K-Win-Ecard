b4286a45528c4209175a2aa8c844ba7bb7f50669
kwinproduction <kwinproduction@gmail.com>
2026-04-30 19:36:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,24 +2,28 @@ const CACHE_NAME = 'K-Win';
 const assets = [
   './',
   './index.html',
-  'https://github.com/kwinproduction/K-Win-Ecard/main/logo.jpeg'
+  // GitHub Raw link එක භාවිතා කර ඇත
+  'https://raw.githubusercontent.com/kwinproduction/K-Win-Ecard/main/logo.jpeg'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// Install Event - Assets Cache කිරීම
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('K-Win: Caching assets');
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
-
-// Activate and Clean Old Caches
+// Activate Event - පරණ Caches ඉවත් කිරීම
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
+            console.log('K-Win: Clearing old cache');
             return caches.delete(cache);
           }
         })
@@ -28,7 +32,7 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Fetch Assets from Cache or Network
+// Fetch Event - Offline වැඩ කිරීමට Cache එකෙන් දත්ත ලබා දීම
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
