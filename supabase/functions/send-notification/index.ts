a8f0de478cb3efd1909aa65a801cfa04487cb61b
kwinproduction <kwinproduction@gmail.com>
2026-06-10 22:06:33 +0530

    Update index.ts

--- supabase/functions/send-notification/index.ts
+++ supabase/functions/send-notification/index.ts
@@ -3,6 +3,7 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
 
+
 Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: corsHeaders });
